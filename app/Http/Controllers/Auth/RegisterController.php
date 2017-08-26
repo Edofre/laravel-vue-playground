c260770fed78f359bd86d763529bbb71770c66ce
Edo Freriks <edofre@gmail.com>
2017-08-26 22:37:50 +0200

    Redirect to proper routes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      * Where to redirect users after registration.
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -28,7 +28,7 @@ class ResetPasswordController extends Controller
      * Where to redirect users after resetting their password.
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
