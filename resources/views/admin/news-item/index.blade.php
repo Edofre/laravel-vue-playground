1204bd71c167f964446d80c253fdbba3e02e98f5
Edo Freriks <edofre@gmail.com>
2017-09-09 23:10:26 +0200

    News items index page

---
+++ resources/views/admin/news-item/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('title')
+    {{ __('crud.view_all', ['model' => lcfirst(__('news-item.news_items'))]) }}
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="container">
+            <div class="content">
+                <table class="table is-striped is-narrow">
+                    <thead>
+                    <tr>
+                        <th><abbr title="{{ __('crud.id_description') }}">{{ __('crud.id') }}</abbr></th>
+                        <th>{{ trans('news-item.title') }}</th>
+                        <th>{{ trans('news-item.public') }}</th>
+                        <th><abbr title="{{ trans('crud.actions') }}"></abbr></th>
+                    </tr>
+                    </thead>
+                    <tfoot align="right">
+                    <tr>
+                        <td colspan="4">{{ trans('crud.count_models', ['count' => $newsItems->total()]) }}</td>
+                    </tr>
+                    </tfoot>
+                    <tbody>
+                    @if(!$newsItems->isEmpty())
+                        @foreach($newsItems as $newsItem)
+                            <tr>
+                                <td>{{ $newsItem->id }}</td>
+                                <td>{{ $newsItem->title }}</td>
+                                <td>@boolean($newsItem->public)</td>
+                                <td>
+                                    <div class="field has-addons">
+                                        <p class="control">
+                                            <a href="{!! route('admin.news-item.show', [$newsItem->id]) !!}" title="{{ trans('crud.show') }}" class='button is-primary'>
+                                                <span class="icon icon-black"><i class="fa fa-eye"></i></span>
+                                            </a>
+                                        </p>
+                                        <p class="control">
+                                            <a href="{!! route('admin.news-item.edit', [$newsItem->id]) !!}" title="{{ trans('crud.edit') }}" class="button is-warning">
+                                                <span class="icon"><i class="fa fa-pencil"></i></span>
+                                            </a>
+                                        </p>
+                                        <p class="control">
+                                            {!! Form::open(['route' => ['admin.news-item.destroy', $newsItem->id], 'method' => 'delete']) !!}
+                                            {!! Form::button('<span class="icon icon-black"><i class="fa fa-trash"></i></span>', ['type' => 'submit', 'title'=> trans('crud.delete'),'class' => 'button is-danger', 'onclick' => "return confirm('".trans('crud.are_you_sure')."')"]) !!}
+                                            {!! Form::close() !!}
+                                        </p>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td>
+                                {{ trans('crud.no_models_found', ['model'=> lcfirst(trans('news-item.news_items'))]) }}
+                            </td>
+                        </tr>
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+            {{ $newsItems->appends(!empty($search) ? ['search' => $search] : null)->links() }}
+        </div>
+    </section>
+    {{--<h1 class="panel-title pull-left">{{ trans('crud.view_all', ['model'=> strtolower(trans('news-item.news_items'))]) }}</h1>--}}
+    {{--<a class="btn btn-primary" href="{!! route('admin.news-item.create') !!}">{{ trans('crud.create') }}</a>--}}
+@endsection
