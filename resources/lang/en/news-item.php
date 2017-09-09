73fca0a263737171b5fbeb4ba26b9e7042902670
Edo Freriks <edofre@gmail.com>
2017-09-09 23:12:09 +0200

    Newsitem language file

---
+++ resources/lang/en/news-item.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'news_item'  => 'News item',
+    'news_items' => 'News items',
+
+    'title'                        => 'title',
+    'title_placeholder'            => 'The title',
+    'news_category_id'             => 'News category',
+    'news_category_id_placeholder' => 'The related news category',
+    'message'                      => 'Message',
+    'message_placeholder'          => 'Give the item a message',
+    'public'                       => 'Public',
+    'public_placeholder'           => 'Is the news item available to the public',
+];
