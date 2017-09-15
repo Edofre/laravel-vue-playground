f47aba86417f0d1d4ac0d56ed9e860a02b9487cd
Edo Freriks <edofre@gmail.com>
2017-09-16 00:15:02 +0200

    Edit/delete buttons show page

--- resources/views/admin/news-item/show.blade.php
+++ resources/views/admin/news-item/show.blade.php
@@ -7,6 +7,26 @@
 @section('content')
     <section class="section">
         <div class="container">
+            <!-- Main container -->
+            <nav class="level">
+                <div class="level-left">
+                </div>
+                <div class="level-right">
+                    <div class="field has-addons">
+                        <p class="control">
+                            <a href="{!! route('admin.news-item.edit', [$newsItem->id]) !!}" title="{{ trans('crud.edit') }}" class="button is-warning">
+                                {{ __('crud.edit') }}&nbsp;&nbsp;<span class="icon"><i class="fa fa-pencil"></i></span>
+                            </a>
+                        </p>
+                        <p class="control">
+                            {!! Form::open(['route' => ['admin.news-item.destroy', $newsItem->id], 'method' => 'delete']) !!}
+                            {!! Form::button(__('crud.delete'). '&nbsp;&nbsp;<span class="icon icon-black"><i class="fa fa-trash"></i></span>', ['type' => 'submit', 'title'=> trans('crud.delete'),'class' => 'button is-danger', 'onclick' => "return confirm('".trans('crud.are_you_sure')."')"]) !!}
+                            {!! Form::close() !!}
+                        </p>
+                    </div>
+                </div>
+            </nav>
+
             <div class="content">
                 <dl>
                     <dt><strong>{{ __('crud.id') }}</strong></dt>
@@ -33,6 +53,4 @@
             </div>
         </div>
     </section>
-    {{--<h1 class="panel-title pull-left">{{ trans('crud.view_all', ['model'=> strtolower(trans('news-items.news_items'))]) }}</h1>--}}
-    {{--<a class="btn btn-primary" href="{!! route('admin.news-items.create') !!}">{{ trans('crud.create') }}</a>--}}
 @endsection
