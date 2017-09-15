e44b1966ed4872557800788de23c518932fe3e56
Edo Freriks <edofre@gmail.com>
2017-09-15 23:47:58 +0200

    News tag language file

---
+++ resources/lang/en/news-tag.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'news_tag'  => 'News tag',
+    'news_tags' => 'News tags',
+
+    'name'                    => 'Name',
+    'name_placeholder'        => 'The tag name',
+    'description'             => 'Description',
+    'description_placeholder' => 'Give the tag a description',
+    'public'                  => 'Public',
+    'public_placeholder'      => 'Is the tag available to the public',
+];
