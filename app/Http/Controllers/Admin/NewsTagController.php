63eb703fef6807fa441f40b68d3f9260c39e5936
Edo Freriks <edofre@gmail.com>
2017-09-15 23:44:50 +0200

    NewsTagController

--- app/Http/Controllers/Admin/NewsTagController.php
+++ app/Http/Controllers/Admin/NewsTagController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Store\StoreNewsTagRequest;
+use App\Http\Requests\Admin\Update\UpdateNewsTagRequest;
+use App\Models\NewsTag;
 
 /**
  * Class NewsTagController
@@ -16,7 +18,11 @@ class NewsTagController extends Controller
      */
     public function index()
     {
-        //
+        $newsTags = NewsTag::query()->orderBy('id', 'desc')->paginate(20);
+
+        return view('admin.news-tag.index', [
+            'newsTags' => $newsTags,
+        ]);
     }
 
     /**
@@ -25,57 +31,81 @@ class NewsTagController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.news-tag.create')->with([
+            'newsTag' => new NewsTag(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
-     * @param  \Illuminate\Http\Request $request
+     * @param StoreNewsTagRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreNewsTagRequest $request)
     {
-        //
+        $newsTag = NewsTag::create([
+            'name'        => $request->input('name'),
+            'description' => $request->input('description'),
+            'public'      => $request->input('public', 0),
+        ]);
+
+        //        flash(__('news-tag.created', ['name' => $newsTag->name]))->success()->important();
+        return redirect()->route('admin.news-tag.index');
     }
 
     /**
      * Display the specified resource.
-     * @param  int $id
+     * @param NewsTag $newsTag
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(NewsTag $newsTag)
     {
-        //
+        return view('admin.news-tag.show')->with([
+            'newsTag' => $newsTag,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
-     * @param  int $id
+     * @param NewsTag $newsTag
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(NewsTag $newsTag)
     {
-        //
+        return view('admin.news-tag.edit')->with([
+            'newsTag' => $newsTag,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
-     * @param  \Illuminate\Http\Request $request
-     * @param  int                      $id
+     * @param UpdateNewsTagRequest|\Illuminate\Http\Request $request
+     * @param NewsTag                                       $newsTag
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateNewsTagRequest $request, NewsTag $newsTag)
     {
-        //
+        $newsTag->update([
+            'name'        => $request->input('name'),
+            'description' => $request->input('description'),
+            'public'      => $request->input('public', 0),
+        ]);
+
+
+        //        flash(__('news-tag.updated', ['name' => $newsTag->name]))->success()->important();
+        return redirect()->route('admin.news-tag.index');
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param  int $id
+     * @param NewsTag $newsTag
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(NewsTag $newsTag)
     {
-        //
+        $newsTag->delete();
+
+        //        flash(__('news-tag.deleted', ['name' => $newsTag->name]))->success()->important();
+        return redirect()->route('admin.news-tag.index');
     }
 }
